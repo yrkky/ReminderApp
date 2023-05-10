680e2befd0d9735033a21036653a7467c18d84ab
Janne Yrjänäinen <janne.yrjanainen@gmail.com>
2023-05-10 18:33:24 +0300

    AddReminder Screen Work + Localization (en + fi)

--- app/src/main/java/com/syrjakoyrjanai/reminderapp/ui/reminder/AddReminderScreen.kt
+++ app/src/main/java/com/syrjakoyrjanai/reminderapp/ui/reminder/AddReminderScreen.kt
@@ -1,27 +1,40 @@
 package com.syrjakoyrjanai.reminderapp.ui.reminder
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.Context
+import android.util.Log
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.EditCalendar
 import androidx.compose.material.icons.filled.Save
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.icons.outlined.ArrowDropUp
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.accompanist.insets.systemBarsPadding
+import com.syrjakoyrjanai.core.domain.entity.Category
 import com.syrjakoyrjanai.reminderapp.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 @Composable
@@ -82,12 +95,27 @@ fun AddReminder(
                         Spacer(modifier = Modifier.width(10.dp))
 
                         // Back
-                        Text(stringResource(R.string.back))
+                        Text(
+                            text = stringResource(R.string.back),
+                            color = Color.Black,
+                            fontSize = 16.sp
+                        )
                     }
 
 
                     Button(
-                        onClick = { navigationController.popBackStack() },
+                        onClick = {
+                            // TODO: Save the reminder
+                            navigationController.popBackStack()
+
+                            if (buttonEnabled()) {
+                                // Save the reminder
+                            } else {
+                                // Some kind of alert why can't save
+                                toastError(context)
+                            }
+
+                        },
                         colors = ButtonDefaults.buttonColors(
                             backgroundColor = Color(
                                 255,
@@ -104,7 +132,7 @@ fun AddReminder(
                         // Save
                         Text(
                             text = stringResource(R.string.save),
-                            color = Color.Black,
+                            color = if (buttonEnabled()) Color.Black else Color(0,0,0,120),
                             fontSize = 16.sp
                         )
 
@@ -113,12 +141,259 @@ fun AddReminder(
                         Icon(
                             imageVector = Icons.Default.Save,
                             contentDescription = null,
-                            tint = Color.Black,
+                            tint = if (buttonEnabled()) Color.Black else Color(0,0,0,120),
                         )
                     }
                 }
             }
+
+            // Actual content
+
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .offset((-16).dp),
+                    value = reminderTitle.value,
+                    onValueChange = { reminderTitle_ -> reminderTitle.value = reminderTitle_ },
+                    placeholder = {
+                        Text(
+                        text = stringResource(R.string.reminder_title),
+                        fontSize = 32.sp,
+                        )
+                    },
+                    shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+                    textStyle = TextStyle(
+                        fontSize = 32.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    colors = TextFieldDefaults.textFieldColors(
+                        textColor = Color.Black,
+                        disabledTextColor = Color.Transparent,
+                        backgroundColor = Color.White,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        focusedLabelColor = Color.Transparent,
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    text = stringResource(R.string.reminder_date),
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.Start)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                DatePicker(
+                    remindtime = reminderTime,
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    text = stringResource(R.string.reminder_category),
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.Start)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                CategoryListDropdown(
+                    viewModel = viewModel,
+                    category = reminderCategory
+                )
+
+                Spacer(modifier = Modifier.height(300.dp))
+
+                Button(
+                    onClick = {
+                        // TODO: Save reminder
+                        navigationController.navigate("home")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    shape = RoundedCornerShape(corner = CornerSize(10.dp)),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color(217, 217, 217, 255)),
+                    enabled = buttonEnabled()
+                ) {
+                    Text(stringResource(R.string.save))
+                }
+
+
+            }
+
         }
     }
 
 }
+
+fun toastError(context: Context) {
+    Toast.makeText(
+        context,
+        context.getString(R.string.fill_error),
+        Toast.LENGTH_LONG
+    ).show()
+}
+
+
+@Composable
+private fun CategoryListDropdown(
+    viewModel: MainViewModel,
+    category: MutableState<String>
+) {
+    val categoryState = viewModel.categories.collectAsState()
+    var categoryState_withoutAll = categoryState.value.toMutableSet()
+    Log.i("CategoryListDropdown", "categorylist: ${categoryState}")
+    categoryState_withoutAll.remove(Category(categoryId=1, name="All"))
+    Log.i("CategoryListDropdown", "categorylist_withoutAll: ${categoryState_withoutAll}")
+
+
+    var expanded by remember { mutableStateOf(false) }
+    val icon = if (expanded) {
+        Icons.Outlined.ArrowDropUp // requires androidx.compose.material:material-icons-extended dependency
+    } else {
+        Icons.Filled.ArrowDropDown
+    }
+
+    Column {
+        OutlinedTextField(
+            value = category.value,
+            onValueChange = { category.value = it},
+            modifier = Modifier.fillMaxWidth(),
+            // label = { Text(text = stringResource(R.string.reminder_choose_category)) },
+            placeholder = {
+                Text(
+                    text = stringResource(R.string.reminder_choose_category),
+                    color = Color.Black,
+                )
+            },
+            readOnly = true,
+            trailingIcon = {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null,
+                    modifier = Modifier.clickable { expanded = !expanded }
+                )
+            }
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            categoryState_withoutAll.forEach { dropDownOption ->
+                DropdownMenuItem(
+                    onClick = {
+                        category.value = dropDownOption.name
+                        expanded = false
+                    }
+                ) {
+                    Text(
+                        text = dropDownOption.name,
+                        color = Color.Black,
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+private fun DatePicker(
+    viewModel: MainViewModel = hiltViewModel(),
+    remindtime: MutableState<String>
+) {
+    var localContext = LocalContext.current
+    var mDate = remember { mutableStateOf("") }
+    var mTime = remember { mutableStateOf("") }
+    var mDatetime = remember { mutableStateOf("") }
+
+    var mYear: Int
+    var mMonth: Int
+    var mDay: Int
+    var mHour: Int
+    var mMinute: Int
+    var mCalendar = Calendar.getInstance()
+
+    mYear = mCalendar[Calendar.YEAR]
+    mMonth = mCalendar[Calendar.MONTH]
+    mDay = mCalendar[Calendar.DAY_OF_MONTH]
+    mHour = mCalendar[Calendar.HOUR_OF_DAY]
+    mMinute = mCalendar[Calendar.MINUTE]
+    mCalendar.time = Date()
+
+    var mDatePickerDialog = DatePickerDialog(
+        localContext,
+        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+            mDate.value = "$mDayOfMonth.${mMonth + 1}.$mYear"
+        }, mYear, mMonth, mDay
+    )
+
+    val mTimePickerDialog = TimePickerDialog(
+        localContext,
+        { _, mHour: Int, mMinute: Int ->
+            mTime.value = "$mHour.$mMinute"
+        }, mHour, mMinute, true
+    )
+
+    mDatetime.value = mDate.value + " " + mTime.value
+    // LocalDateTime.of(mYear, mMonth, mDay, mHour, mMinute).toString()
+
+    if (mDate.value.isNotEmpty() && mTime.value.isNotEmpty()) {
+        remindtime.value = LocalDateTime.of(
+            mDate.value.split(".")[2].toInt(),
+            mDate.value.split(".")[1].toInt(),
+            mDate.value.split(".")[0].toInt(),
+            mTime.value.split(".")[0].toInt(),
+            mTime.value.split(".")[1].toInt(),
+        ).toString()
+
+        mDatetime.value = LocalDateTime
+            .parse(remindtime.value)
+            .format(
+                DateTimeFormatter
+                    .ofPattern("dd.MM.yyyy HH.mm")
+            )
+    }
+
+    Column(modifier = Modifier.fillMaxWidth()
+    ) {
+
+        OutlinedTextField(
+            value = mDatetime.value.ifBlank { stringResource(R.string.reminder_choose_date) },
+            modifier = Modifier.fillMaxWidth(),
+            // label = { Text(text = stringResource(R.string.reminder_date)) },
+            onValueChange = { mDatetime.value = it},
+            readOnly = true,
+            placeholder = { Text(text = stringResource(R.string.reminder_choose_date)) },
+            trailingIcon = {
+                IconButton(onClick = {
+                    mTimePickerDialog.show()
+                    mDatePickerDialog.show()
+                })
+                {
+                    Icon(
+                        imageVector = Icons.Filled.EditCalendar,
+                        contentDescription = null,
+                        tint = Color(0, 0, 0, 255)
+                    )
+                }
+            },
+        )
+
+    }
+}
+
+
+
+
+
+
